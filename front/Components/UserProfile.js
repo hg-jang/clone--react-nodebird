8699c0378496fd84ddfc7db28b5483d0a96df798
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-10-15 17:09:54 +0900

    링크 추가

--- front/Components/UserProfile.js
+++ front/Components/UserProfile.js
@@ -2,6 +2,7 @@ import { useCallback } from 'react'
 import { Card, Avatar, Button } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import { logoutRequestAction } from "../reducers/user"
+import Link from 'next/link'
 
 const UserProfile = () => {
   const dispatch = useDispatch()
@@ -14,12 +15,12 @@ const UserProfile = () => {
   return (
     <Card
       actions={[
-        <div key="twit">짹짹<br />{me.Posts.length}</div>,
-        <div key="followings">팔로잉<br />{me.Followings.length}</div>,
-        <div key="followings">팔로워<br />{me.Followers.length}</div>
+        <div key="twit"><Link href={`/user/${me.id}`}><a>짹짹<br />{me.Posts.length}</a></Link></div>,
+        <div key="followings"><Link href="profile"><a>팔로잉<br />{me.Followings.length}</a></Link></div>,
+        <div key="followings"><Link href="profile"><a>팔로워<br />{me.Followers.length}</a></Link></div>
       ]}
     >
-      <Card.Meta title={me.nickname} avatar={<Avatar>{me.nickname[0]}</Avatar>} />
+      <Card.Meta title={me.nickname} avatar={<Link href={`/user/${me.id}`}><a><Avatar>{me.nickname[0]}</Avatar></a></Link>} />
       <Button onClick={onLogOut} loading={logOutLoading}>로그아웃</Button>
     </Card>
   )
